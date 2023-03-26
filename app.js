f6c2077d53ed27eea7f81fc36d326b589a074399
brickphone <mattis.hutt@gmail.com>
2023-03-26 15:25:09 +0200

    bunchostuff

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
-const numberKeys = document.getElementById("number-button")
-const subtract = document.getElementById("subtract")
+const numberKeys = document.querySelectorAll('.number-key');
+const operatorKeys = document.querySelectorAll('.operator-key');
+const result = document.querySelector('.result');
 
-let num1 = 0;
-let num2 = 0;
+let num1 = "";
+let num2 = "";
+let operator = '';
+let isFirstNum = true;
+
+function numDisplay() {
+  numberKeys.forEach(key => {
+    key.addEventListener("click", () => {
+      if (isFirstNum) {
+        num1 += key.innerHTML;
+        result.innerHTML = num1;
+      } else {
+        num2 += key.innerHTML;
+        result.innerHTML = num2;
+      }
+    });
+  });
+}
+
+function setOperator 
+operatorKeys.forEach(key => {
+  key.addEventListener("click", () => {
+    operator = key.innerHTML;
+    isFirstNum = false;
+  })
+});
 
 function operate() {
 
