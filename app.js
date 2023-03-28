5794c6b7e979993ea1cec98a3516a66553d51cba
brickphone <mattis.hutt@gmail.com>
2023-03-28 15:58:25 +0200

    working final

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
 const numberKeys = document.querySelectorAll('.number-key');
 const operatorKeys = document.querySelectorAll('.operator-key');
+const equalsKey = document.querySelector('#equals');
 const result = document.querySelector('.result');
-
-//TODO : fix game logic
 
 let num1 = "";
 let num2 = "";
-let operator = '';
+let operator = "";
 let isFirstNum = true;
 
 function startingPoint() {
-  result.innerHTML = '0';
-  num1 = '';
-  num2 = '';
-  operator = '';
+  result.innerHTML = "0";
+  num1 = "";
+  num2 = "";
+  operator = "";
   isFirstNum = true;
 }
 
 function numDisplay() {
-  numberKeys.forEach(key => {
+  numberKeys.forEach((key) => {
     key.addEventListener("click", () => {
       if (isFirstNum) {
         num1 += key.innerHTML;
@@ -31,13 +30,21 @@ function numDisplay() {
   });
 }
 
+equalsKey.addEventListener('click', () => {
+  operate(num1, num2, operator);
+});
+
 function setOperator() {
-  operatorKeys.forEach(key => {
-    key.addEventListener("click", () => {
+  operatorKeys.forEach((key) => {
+    key.addEventListener('click', () => {
+      if (key.innerHTML === '=') {
+        return;
+      }
+
       operator = key.innerHTML;
       isFirstNum = false;
       operatorDisplay();
-    })
+    });
   });
 }
 
@@ -46,35 +53,43 @@ function operatorDisplay() {
 }
 
 function clearDisplay() {
-  const clearBtn = document.querySelector('#clear')
+  const clearBtn = document.querySelector("#clear");
   clearBtn.addEventListener("click", () => {
     startingPoint();
-  })
+  });
 }
 
-function calculate() {
-  const totalSum = operate(num1, num2, operator);
-  result.innerHTML = totalSum;
-}
+function operate() {
+  console.log(operator)
+  let total = 0;
 
-function operate(num1, num2, operator) {
-  let totalSum = 0;
-  
-  if (operator === '+') {
-    totalSum = parseFloat(num1) + parseFloat(num2);
-  } else if (operator === '-') {
-    totalSum = parseFloat(num1) - parseFloat(num2);
-  } else if (operator === '*') {
-    totalSum = parseFloat(num1) * parseFloat(num2);
-  } else if (operator === '/') {
-    totalSum = parseFloat(num1) / parseFloat(num2);
-  } else if (operator === '=') {
-    totalSum = parseFloat(num2);
+  switch (operator) {
+    case '+':
+      total = parseFloat(num1) + parseFloat(num2);
+      break;
+    case '-':
+      total = parseFloat(num1) - parseFloat(num2);
+      break;
+    case '*':
+      total = parseFloat(num1) * parseFloat(num2);
+      break;
+    case '/':
+      total = parseFloat(num1) / parseFloat(num2);
+      break;
+    default:
+      console.log('Invalid operator');
+      break;
   }
-  
-  return totalSumM;
-};
+
+  result.innerHTML = total;
+  num1 = total;
+  num2 = "";
+  operator = "";
+  isFirstNum = true;
+}
 
 numDisplay();
 setOperator();
 clearDisplay();
+
+
